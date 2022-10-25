99bc00d25ca7cc866746b3628ad4cd052186795a
rafa <ralfrr55@gmail.com>
2022-10-25 11:12:51 -0300

    componenetes home about

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # curso-angular-2
 
-rodar backend 
-node ace serve
-node ace generate:key
-node ace migration:run
+rodar backend <br/>
+node ace serve <br/>
+node ace generate:key <br/>
+node ace migration:run <br/>
 
 
--- moments/src/app/app.module.ts
+++ moments/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './compenents/header/header.component';
+import { FooterComponent } from './compenents/footer/footer.component';
+import { AboutComponent } from './compenents/pages/about/about.component';
+import { HomeComponent } from './compenents/pages/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
